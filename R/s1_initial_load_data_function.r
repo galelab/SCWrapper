2e307b8955844cf26bbb1b072f4a3274598c6a59
Leanne <leanne382@gmail.com>
2020-03-26 08:48:52 -0700

    update R/s1_initial_load_data_function.r to load downsampled and normal 10x data and preprocess it (not functional yet)

---
+++ R/s1_initial_load_data_function.r
@@ -0,0 +1,114 @@
+#' Initially load data into Seaurat
+#'
+#' This generates new folder (internal function only)
+#' @param path_10x_data to generate
+#' @param lower_gene_filter_num minimum number of genes that has to be detected in a cell 
+#' @param upper_gene_filter_num maximum number of genes that can be detected in a cell
+#' @param percent_mito allowable percenatage of mitochondria DNA allowed
+#' @param downsampled if downsampled feature was used when cell ranger was run
+#' @param results_folder folder that results appear in (default: s1_quality_control_results)
+#' @import Seurat
+#' @import stringr
+#' @import rlist
+#' @keywords load data
+#' @export
+
+s1_load_data <- function(path_10_data, sample_names,
+                         lower_gene_filter_num=200,
+                         upper_gene_filter_num=2500,
+                         percent_mito=10, downsampled=TRUE,
+                         celltype_garnett=TRUE,
+                         results_folder="s1_quality_control_results") {
+    results_path <- generate_folder(results_folder)
+    unlink(paste0(results_folder, "/*"))
+
+    if (downsampled.isTRUE()) {
+        print("STATUS: Read 10x downsampled object")
+        pbmc_data <- Read10X(data.dir = path_10_data)
+
+        path_to_library <- str_remove(
+            path_10_data,
+            "filtered_feature_bc_matrix/"
+        )
+
+        print("STATUS: Load library information")
+        libraries <- read.csv(paste0(path_to_library, "aggregation.csv"))
+        ids <- rownames(libraries)
+        samplenames <- as.character(libraries$library_id)
+        print("STATUS: Create Seurat object")
+        pbmc <- CreateSeuratObject(
+            counts = pbmc_data, names.field = 2,
+            names.delim = "-",
+            min.cells = 1, min.features = lower_gene_filter_num
+        )
+        print("STATUS: Assign cell identities")
+        pbmc$sample <- plyr::mapvalues(
+            x = pbmc$orig.ident,
+            from = ids,
+            to = samplenames
+        )
+    } else {
+        counter <- 1
+        pbmc_list <- list()
+        for (path in path_10_data) {
+            pbmc_data <- Read10X(data.dir = path)
+            pbmc <- CreateSeuratObject(counts = pbmc_data,
+                project = sampleID,
+                min.cells = 1,
+                min.features = lower_gene_filter_num
+            )
+            list.append(pbmc_list, sample_names[counter] = pbmc)
+            counter <- counter + 1
+        }
+        anchors <- FindIntegrationAnchors(object.list = pbmc_list, dims = 1:20)
+        pbmc <- IntegrateData(anchorset = anchors, dims = 1:20)
+    }
+    pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+    VlnPlot(pbmc,
+        features = c("nFeature_RNA", "nCount_RNA", "percent.mt"),
+        ncol = 3, group.by = "sample", pt.size = 0.8
+    )
+    ggsave(paste0(results_path, "VnPlotMt.png"), dpi = 300)
+    
+    pbmc <- subset(pbmc,
+       subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 10
+    )
+    
+    VlnPlot(pbmc,
+        features = c("nFeature_RNA", "nCount_RNA", "percent.mt"),
+        group.by = "sample", pt.size = 0.8,
+        ncol = 3
+    )
+    ggsave(paste0(results_path, "VnPlotMt_Filtered.png"), dpi = 300)
+
+    if (celltype_garnett.isTRUE()) {
+        
+    }
+
+    pbmc <- ScaleData(pbmc, vars.to.regress = "percent.mt", features = all.genes, verbose = FALSE)
+    all.genes <- rownames(pbmc)
+
+    pbmc <- NormalizeData(pbmc,
+        normalization.method = "LogNormalize",
+        scale.factor = 10000
+    )
+
+    pbmc <- FindVariableFeatures(pbmc,
+        selection.method = "vst",
+        nfeatures = 2000
+    )
+
+    pbmc <- ScaleData(pbmc,
+        vars.to.regress = "percent.mt",
+        features = all.genes, verbose = FALSE
+    )
+    pbmc <- RunPCA(pbmc, npcs = 30, verbose = FALSE)
+    pbmc <- RunUMAP(pbmc, reduction = "pca", dims = 1:20)
+
+    DimPlot(pbmc_norm, reduction = "umap", group.by = "sample")
+    ggsave(paste0(results_path, "UMAP_sample.png"), dpi = 300)
+
+    saveRDS(pbmc, paste0(results_path, "SC_pbmc_norm_object.rds"))
+
+}
