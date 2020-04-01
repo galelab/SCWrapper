192b71712b33d6686ad3827a847799f4022a4439
Leanne <leanne382@gmail.com>
2020-03-31 21:32:29 -0700

    fix bugs in R/s2_cellclassify_garnett_function.r also generate more umap figures

--- R/s2_cellclassify_garnett_function.r
+++ R/s2_cellclassify_garnett_function.r
@@ -5,22 +5,27 @@
 #' @param path_10x_data path to 10x folder
 #' @param lower_gene_filter_num minimum number of genes that has to be detected in a cell
 #' @param marker_file_path path to the marker file needed for garnett classification
-#' @param pbmc_classifier path to pre built classifier if one is being used (defualt is to build its own classifier)
+#' @param classifier path to pre built classifier if one is being used (defualt is to build its own classifier)
 #' @param cds_gene_type type of gene ID (defualt is Ensembl)
+#' @param marker_file_gene_type type of gene ID in marker file (marker_file_path) (defualt is SYMBOL)
 #' @param save_object save seurat normalized object in RDS file so this step doesn't have to be redone everytime (defualt is TRUE)
 #' @param results_folder path to results of analysis (defualt is s2_garnett_classifier_results)
 #' @import Seurat
 #' @import monocle3
 #' @import garnett
 #' @import dplyr
+#' @import stringr
 #' @import SingleCellExperiment
+#' @import data.table
 #' @keywords cell type classification using garnett classifier
 #' @export
 
-classify_pbmc_cells <- function(seurat_data, path_10x_data, sample_names = FALSE,
+s2_classify_cells <- function(seurat_data, path_10x_data, sample_names = FALSE,
                                 marker_file_path, lower_gene_filter_num = 200,
-                                pbmc_classifier = FALSE,
-                                cds_gene_type = "ENSEMBL", downsampled = TRUE,
+                                classifier = FALSE,
+                                cds_gene_type = "ENSEMBL",
+                                marker_file_gene_type="SYMBOL",
+                                downsampled = TRUE,
                                 save_object = TRUE,
                                 results_folder = "s2_garnett_classifier_results") {
     results_path <- generate_folder(results_folder)
@@ -43,9 +48,10 @@ classify_pbmc_cells <- function(seurat_data, path_10x_data, sample_names = FALSE
     } else {
         counter <- 1
         pbmc_list <- list()
+        barcode_list <- list()
         for (path in path_10x_data) {
             print(paste0("STATUS: Loading ", path, " ", sample_names[counter]))
-            SAMPLE <- load_cellranger_data(sample_path,
+            SAMPLE <- load_cellranger_data(path,
                 umi_cutoff = lower_gene_filter_num
             )
 
@@ -57,16 +63,32 @@ classify_pbmc_cells <- function(seurat_data, path_10x_data, sample_names = FALSE
                 cell_metadata = cell_meta_data,
                 gene_metadata = gene_meta_data
             )
+            alteredbarcodes <- c()
+            barcodes <- colnames(cds)
+            for (i in 1:(length(barcodes))) {
+                alteredbarcodes[i] <- str_replace(barcodes[i], "1", as.character(counter))
+            }
+            colnames(cds) <- alteredbarcodes
             pbmc_list[[sample_names[counter]]] <- cds
+            barcode_list[[sample_names[counter]]] <- alteredbarcodes
+            counter <- counter + 1
         }
         cds <- combine_cds(pbmc_list)
     }
+    ###Adjust cell barcodes to match Seurat formating
+    cds_new_barcodes <- c()
+    cds_barcodes <- colnames(cds)
+    for (i in 1:(length(cds_barcodes))) {
+        cds_new_barcodes[i] <- str_replace(cds_barcodes[i], "_\\S+$", "")
+    }
+    colnames(cds) <- cds_new_barcodes
 
+    ##Adjust Seurat barcodes
     barcodes <- names(seurat_data$percent.mt)
     alteredbarcodes <- c()
 
     for (i in 1:(length(names(seurat_data$percent.mt)))) {
-        alteredbarcodes[i] <- barcodes[i]
+        alteredbarcodes[i] <- str_replace(barcodes[i], "_", "-")
     }
 
     print(paste0(
@@ -74,7 +96,7 @@ classify_pbmc_cells <- function(seurat_data, path_10x_data, sample_names = FALSE
         length(barcodes),
         " should be kept based on Mitochondrial filtering"
     ))
-
+    # print(colnames(cds))
     cds_subset <- cds[, alteredbarcodes]
 
     print("STATUS: Preprocessing step")
@@ -87,8 +109,8 @@ classify_pbmc_cells <- function(seurat_data, path_10x_data, sample_names = FALSE
         preprocess_method = "PCA",
         reduction_method = "UMAP"
     )
-    if (typeof(pbmc_classifier) == "character") {
-        pbmc_classifier <- readRDS(pbmc_classifier)
+    if (typeof(classifier) == "character") {
+        pbmc_classifier <- readRDS(classifier)
     } else {
         pbmc_classifier <- train_cell_classifier(
             cds = cds,
@@ -99,7 +121,7 @@ classify_pbmc_cells <- function(seurat_data, path_10x_data, sample_names = FALSE
             cluster_extend_max_frac_unknown = 1,
             num_unknown = 500,
             verbose = TRUE,
-            marker_file_gene_id_type = "SYMBOL"
+            marker_file_gene_id_type = marker_file_gene_type,
         )
     }
     ## Classify cells with classifer built previously
@@ -115,22 +137,42 @@ classify_pbmc_cells <- function(seurat_data, path_10x_data, sample_names = FALSE
 
     marker_check <- check_markers(rpbmc_cds, marker_file_path,
         db = org.Hs.eg.db,
-        cds_gene_id_type = "ENSEMBL",
-        marker_file_gene_id_type = "SYMBOL"
+        cds_gene_id_type = cds_gene_type,
+        marker_file_gene_id_type = marker_file_gene_type
     )
 
     # Generate marker figure
     plot_markers(marker_check)
-    ggsave(paste0(results_path, "Marker_Fig.png"), dpi = 500)
+    ggsave(paste0(results_path, "/Marker_Fig.png"), dpi = 500)
+
+    visualize_data(rpbmc_cds, results_path, "all")
+
+    seurat_data[["garnett_cluster_extend"]] <- rpbmc_cds$cluster_ext_type_final
+
+    counter <- 1
+    for (barcodes in barcode_list) {
+        overlap_vector <- intersect(as.character(barcodes), colnames(rpbmc_cds))
+        rpbmc_cds_sub <- rpbmc_cds[, as.character(overlap_vector)]
+        visualize_data(rpbmc_cds_sub, results_path, sample_names[counter])
+        counter <- counter + 1
+    }
+
+    if (isTRUE(save_object)) {
+        saveRDS(seurat_data, paste0(results_path, "/SC_pbmc_norm_object.rds"))
+    }
 
 
+    return(rpbmc_cds)
+}
+
+visualize_data <- function(rpbmc_cds, results_path, sample) {
     plot_cells(rpbmc_cds,
         color_cells_by = "cluster_ext_type",
         label_cell_groups = FALSE
     )
 
-    ggsave(paste0(results_path, "Umap_cluster_ext_type.png"),
-        width = 5, height = 5, dpi = 500
+    ggsave(paste0(results_path, "/Umap_cluster_ext_type_", sample, ".png"),
+        width = 6, height = 5, dpi = 500
     )
 
     plot_cells(rpbmc_cds,
@@ -138,25 +180,21 @@ classify_pbmc_cells <- function(seurat_data, path_10x_data, sample_names = FALSE
         color_cells_by = "cell_type",
         label_cell_groups = FALSE
     )
-    ggsave(paste0(results_path, "Umap_cell_type.png"),
-        width = 5, height = 5, dpi = 500
+    ggsave(paste0(results_path, "/Umap_cell_type_", sample, ".png"),
+        width = 6, height = 5, dpi = 500
     )
 
     plot_cells(rpbmc_cds,
         color_cells_by = "cluster_ext_type_final",
         label_cell_groups = FALSE
     )
-    ggsave(paste0(results_path, "Umap_cluster_ext_type_final.png"),
-        width = 5, height = 5,
+    ggsave(paste0(results_path,
+             "/Umap_cluster_ext_type_final_",
+              sample,
+              ".png"),
+        width = 6, height = 5,
         dpi = 500
     )
-
-    seurat_data[["garnett_cluster_extend"]] <- rpbmc_cds$cluster_ext_type_final
-    if (isTRUE(save_object)) {
-        saveRDS(seurat_data, paste0(results_path, "SC_pbmc_norm_object.rds"))
-    }
-
-    return(seurat_data)
 }
 
 final_cds_garnett_classification <- function(cds_garnett) {
